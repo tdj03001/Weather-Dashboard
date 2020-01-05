4a143f7da8b7c2962d7f54268388db00c0dec4e9
Tyler Jenkins <tyjenki@gmail.com>
2020-01-05 16:32:36 -0500

    added event listener for search button. added displayInputCurrentWeather function to do same thing as displayCurrentWeather function but to not be hard coded

--- jquery.js
+++ jquery.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
 function displayCurrentWeather() {
-  var cityName = "boston";
+  var cityName = "Boston";  
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=47dc3b56adc3a5773ac8eaebd8b0c012&units=imperial";
 
   $.ajax({
@@ -21,9 +21,9 @@ function displayCurrentWeather() {
     var pOne = $("<p>").text("City: " + city);
     currentWeather.append(pOne);
 
-    var date = response.dt; //whatever "date" is called
-    var pTwo = $("<p>").text("Date: " + date);
-    currentWeather.append(pTwo);
+    // var date = response.dt; //convert from UTC
+    // var pTwo = $("<p>").text("Date: " + date);
+    // currentWeather.append(pTwo);
 
     var temperature = response.main.temp;
     var pThree = $("<p>").text("Temperature: " + temperature + "  Fahrenheit");
@@ -41,10 +41,60 @@ function displayCurrentWeather() {
     var pSix = $("<p>").text("UV Index: " + uvIndex);
     currentWeather.append(pSix);
 
-  });
+  }); //closes ajax call
 }; //closes displayCurrentWeather function
 
 displayCurrentWeather();
+
+function displayInputCurrentWeather() {
+  var cityName = $("#city").val().trim();  
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=47dc3b56adc3a5773ac8eaebd8b0c012&units=imperial";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    $("#currentWeather").empty();
+
+    var currentWeather = $("#currentWeather").append($("<div class='currentWeather'>"));
+
+    var icon = response.weather[0].icon;
+    var weatherImg = $("<img>").attr("src", ("https://openweathermap.org/img/w/" + icon + ".png"));
+    currentWeather.append(weatherImg);
+
+    var city = response.name; 
+    var pOne = $("<p>").text("City: " + city);
+    currentWeather.append(pOne);
+
+    // var date = response.dt; //convert from UTC
+    // var pTwo = $("<p>").text("Date: " + date);
+    // currentWeather.append(pTwo);
+
+    var temperature = response.main.temp;
+    var pThree = $("<p>").text("Temperature: " + temperature + "  Fahrenheit");
+    currentWeather.append(pThree);
+
+    var humidity = response.main.humidity;
+    var pFour = $("<p>").text("Humidity: " + humidity + "%");
+    currentWeather.append(pFour);
+
+    var windSpeed = response.wind.speed;
+    var pFive = $("<p>").text("Wind Speed: " + windSpeed + " mph");
+    currentWeather.append(pFive);
+    
+    var uvIndex = response.uvindex; //whatever "uv index" is called
+    var pSix = $("<p>").text("UV Index: " + uvIndex);
+    currentWeather.append(pSix);
+
+  }); //closes ajax call
+}; //closes displayInputCurrentWeather function
+
+
+
+$("#citySearch").on("click", function(event) {
+  event.preventDefault();
+  displayInputCurrentWeather();
+}); //closes click event
 
 
 }); //document.ready closing
